c701cde006082e173fad1e770c402f6bb00176b8
ahmdrz <ahmadrezazibaei@hotmail.com>
2017-01-30 18:38:12 +0330

    update - build using sh file

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 all: 
-	@cd server && go build -i -o ../simorgh-server
-	@cd client && go build -i -o ../simorgh-client
+	@sh build.sh
 install: all
 	@cp simorgh-server /usr/local/bin
 	@cp simorgh-client /usr/local/bin
---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+TMP="$(mktemp -d -t simorgh.XXXX)"
+
+function rmtemp {
+	rm -rf "$TMP"
+}
+trap rmtemp EXIT
+
+CURRENT="$(pwd)"
+SIMORGH="$TMP/src/github.com/ahmdrz/simorgh"
+
+export GOPATH="$TMP"
+for file in `find . -type f`; do
+	if [[ "$file" != "." && "$file" != ./.git* && "$file" != ./data* && "$file" != ./summitdb-* ]]; then
+		mkdir -p "$SIMORGH/$(dirname "${file}")"
+		cp -P "$file" "$SIMORGH/$(dirname "${file}")"
+	fi
+done
+
+cd $SIMORGH
+cd server && go build -i -o $CURRENT/simorgh-server
+cd ../client && go build -i -o $CURRENT/simorgh-client
